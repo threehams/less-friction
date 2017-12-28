67b99936a0eec994ed1fa128732a98e5fd735458
threehams <davidaedmondson@gmail.com>
2017-12-28 14:16:53 -0800

    Set up transform directory.

---
+++ packages/less-friction/src/index.test.ts
@@ -0,0 +1,7 @@
+describe("transform", () => {
+  describe("basic class component", () => {
+    it("extracts a stateless component", () => {
+      expect(
+    });
+  });
+});
--- packages/less-friction/src/index.ts
+++ packages/less-friction/src/index.ts
@@ -1,5 +1,6 @@
 import { parse } from "babylon";
 import traverse from "babel-traverse";
+import generate from "babel-generator";
 import t from "babel-types";
 
 interface Options {
@@ -7,10 +8,16 @@ interface Options {
   column: number;
 }
 
-const run = (code, options) => {
+const run = (code: string, options: Options) => {
   const ast = parse(code);
 
   traverse(ast, {
-    ThisExpression: path => {},
+    ThisExpression: path => {
+      path.getBindingIdentifiers();
+    },
   });
+
+  return generate(ast, {}, code);
 };
+
+export default run;
